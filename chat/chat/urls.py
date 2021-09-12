dedd9850b1fd90f9030f279b89158f1bbbfcdc36
Wellington Mendes <wellmend0@gmail.com>
2021-09-12 09:01:07 -0300

    change app name from room to dashboard

--- chat/chat/urls.py
+++ chat/chat/urls.py
@@ -20,5 +20,5 @@ from django.urls.conf import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("registry.urls")),
-    path('dashboard/', include("room.urls"))
+    path('dashboard/', include("dashboard.urls"))
 ]
