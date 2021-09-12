6c399df4dadfd223385b0e02f2ca43ece39d133d
Wellington Mendes <wellmend0@gmail.com>
2021-09-12 10:37:52 -0300

    [#3] structure pages flow

--- chat/dashboard/urls.py
+++ chat/dashboard/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.dashboardView, name='home'),
+    path('rooms', views.dashboardView, name='rooms'),
+    path('chat', views.chatView, name='chat'),
 ]
--- chat/dashboard/views.py
+++ chat/dashboard/views.py
@@ -3,4 +3,13 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def dashboardView(request):
-    return render(request, 'dashboard/home.html')
+    if request.method == "POST":
+        selected_room = request.POST.get('room_button')
+        context = {"room": selected_room}
+        return render(request, 'dashboard/chat/home.html', context)
+    context = {"rooms": []}
+    return render(request, 'dashboard/rooms/home.html', context)
+
+@login_required
+def chatView(request):
+    return render(request, 'dashboard/chat/home.html')
