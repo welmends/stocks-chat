560eb7a95e754ed2f63ab9571bac5daf6f4281b1
Wellington Mendes <wellmend0@gmail.com>
2021-09-13 10:17:04 -0300

    [#11] remove email from sign up form

--- chat/registration/forms.py
+++ chat/registration/forms.py
@@ -3,14 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class UserSignUpForm(UserCreationForm):
-    email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1']
+        fields = ['username', 'password1']
 
     def __init__(self, *args, **kwargs):
         super(UserCreationForm, self).__init__(*args, **kwargs)
         del self.fields['password2']
-        for fieldname in ['username', 'email', 'password1']:
+        for fieldname in ['username', 'password1']:
             self.fields[fieldname].help_text = None
