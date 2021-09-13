c293ee4184a30bab00d989b936c21805306c69da
Wellington Mendes <wellmend0@gmail.com>
2021-09-13 12:11:52 -0300

    [#15] update clean_name validation

--- chat/dashboard/forms.py
+++ chat/dashboard/forms.py
@@ -12,7 +12,8 @@ class CreateRoomForm(ModelForm):
         self.fields['name'].required = False
 
     def clean_name(self):
+        special_characters = '!@#$%^&*()-+?=,_<>/'
         name = self.cleaned_data.get('name')
-        if name.isalpha()==False:
-            raise forms.ValidationError('Room name must not contains special characters')
+        if any(sc in name for sc in special_characters):
+            raise forms.ValidationError('Room name must not contains special characters: {}'.format(special_characters))
         return name
